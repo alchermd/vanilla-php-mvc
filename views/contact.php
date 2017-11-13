7f931ffde8b3a21e4939e4e8d13ac416e69e907f
alchermd <johnalcherdoloiras@gmail.com>
2017-11-14 02:46:02 +0800

    Require repeated template contents.

--- views/contact.php
+++ views/contact.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact :: PHP Basics</title>
-</head>
-<body>
-    <?php require 'views/layouts/nav.php' ?>
-    
-    <h1>Contact Us</h1>
-</body>
-</html>
+<?php require 'views/layouts/top.php' ?>
+
+<h1>Contact</h1>
+<?php require 'views/layouts/bottom.php' ?>
