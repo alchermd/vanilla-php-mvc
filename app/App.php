6157439c7dc15f0ea1d47936093c201e49222264
alchermd <johnalcherdoloiras@gmail.com>
2017-11-14 19:50:06 +0800

    Create App class.

---
+++ app/App.php
@@ -0,0 +1,17 @@
+<?php
+
+// A Dependency-Injection Container
+class App
+{
+    protected static $registry = [];
+
+    public static function bind(string $key, $val)
+    {
+        static::$registry[$key] = $val;
+    }
+
+    public static function get(string $key)
+    {
+        return static::$registry[$key];
+    }
+}
