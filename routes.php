1e332f7b7502a906ec881439aefdca30ffbd931a
alchermd <johnalcherdoloiras@gmail.com>
2017-11-14 21:49:51 +0800

    Add tasks GET route.

--- routes.php
+++ routes.php
@@ -3,4 +3,5 @@
 $router->get('', 'PageController@index');
 $router->get('about', 'PageController@about');
 $router->get('contact', 'PageController@contact');
+$router->get('tasks', 'TaskController@index');
 $router->post('tasks', 'TaskController@store');
